1672a705983a52b729d09d0950444c11d42618a0
silvercricket <37115695+silvercricket@users.noreply.github.com>
2025-02-28 19:31:44 -0800

    game schemas

---
+++ server/db/models/games/index.js
@@ -0,0 +1,5 @@
+const { ClueSession , CluePlayer } = require('./clue.js');
+const { YahtzeeSession, YahtzeePlayer} = require('./yahtzee.js');
+const { ScrabbleSession, ScrabblePlayer} = require('./scrabble.js');
+
+module.exports = { ClueSession, CluePlayer, YahtzeeSession, YahtzeePlayer, ScrabbleSession, ScrabblePlayer };
