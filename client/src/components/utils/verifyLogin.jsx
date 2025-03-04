dd37c5c596d2fca247b50daa639dc210cb94b862
Kevin <kevinvar2805@gmail.com>
2025-03-03 18:32:55 -0600

    Changed file structure and custom hook for verifyLogin

---
+++ client/src/components/utils/verifyLogin.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const VerifyLogin = () => {
+  
+  return (
+    <></>
+  );
+};
+
+export default VerifyLogin;
--- server/auth/firebase.config.js
+++ server/auth/firebase.config.js
@@ -3,7 +3,7 @@ const { initializeApp } = require('firebase/app');
 const admin = require('firebase-admin');
 const { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, signInAnonymously, signOut, updateProfile, setPersistence, inMemoryPersistence } = require('firebase/auth');
 const { getAnalytics } = require('firebase/analytics');
-const serviceAccount = require(process.env.SERVICE_ACCOUNT_URL);
+const serviceAccount = require('./selectionKey/' + process.env.SERVICE_ACCOUNT_URL);
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,16 +18,7 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
-const firebaseAdminConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-  credential: admin.credential.cert(serviceAccount)
-}
+const firebaseAdminConfig = {...firebaseConfig, credential: admin.credential.cert(serviceAccount)}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
