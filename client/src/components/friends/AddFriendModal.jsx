5325ebd5905fe0aa09a79fc9f98ef17bc9ba66c5
Lucas <lucascostamonteiro@yahoo.com>
2025-03-05 09:26:31 -0800

    friends setup

--- client/src/components/friends/AddFriendModal.jsx
+++ client/src/components/friends/AddFriendModal.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const AddFriendModal = ({friends}) => {
-
   const [searchResults, setSearchResults] = useState([]);
 
   const handleSearch = (event) => {
