5271724a185b4350b420af1a8c54328b5d928eae
Kevin <kevinvar2805@gmail.com>
2025-03-05 14:02:53 -0600

    Added react query functionality for live chatting

--- client/src/components/games/messages/MessageBoard.jsx
+++ client/src/components/games/messages/MessageBoard.jsx
@@ -5,14 +5,15 @@ import useVerifyLogin from '../../utils/useVerifyLogin.jsx'
 
 
 const MessageBoard = ({ gameId }) => {
-  console.log('message board');
+  // const {messages} = useQuery({
+  //   queryKey: ['messages']
+  // })
   const { loggedIn, email } = useVerifyLogin(false);
-  const [messages, setMessages] = useState([]);
+
   const getMessages = () => {
-    console.log('something')
-    axios.get('/api/messages/' + gameId)
+    return axios.get('/api/messages/' + gameId)
     .then((messages) => {
-      setMessages(messages.data);
+      return messages.data;
     })
   }
 
@@ -20,23 +21,31 @@ const MessageBoard = ({ gameId }) => {
     e.preventDefault();
     const formdata = new FormData(e.target);
     const message = formdata.get('message');
-    axios.post('/api/messages', {gameId, email, message})
-      .then(() => getMessages())//keep this HERE, very important
-      .catch((err) => console.error(err))
+    axios.post('/api/messages', {gameId, email, message});
+      // .then(() => getMessages())//keep this HERE, very important
+      // .catch((err) => console.error(err))
   }
 
-  useEffect(() => {
-    getMessages();
-  }, [])
+  // useEffect(() => {
+  //   getMessages();
+  // }, [])
+
+  const {data, isLoading, error} = useQuery({
+    queryKey: ['messages'],
+    queryFn: getMessages,
+    refetchInterval: 1000,
+
+  })
+
   return (
     <div className="flex flex-col mt-5 bg-base-200 border border-primary p-4 fixed bottom-0 left-0 right-0 max-h-4/10">
-      {console.log(messages)}
+      {console.log(data)}
       <div className="flex-grow overflow-y-scroll">
-        {messages.map(message => (
+        {data ? data.map(message => (
           <div className="border bg-base-300" key={message.id}>
             <p className="chat-bubble">{message.message}</p>
           </div>
-        ))}
+        )) : <div>Loading...</div>}
       </div>
       <div className="divider"/>
       <form className="flex-shrink-0" onSubmit={postMessage}>
