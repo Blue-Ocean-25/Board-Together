5c9517a649c2060eeec9c686d24f7c2e85500fef
Byron Work <workbyron22@gmail.com>
2025-03-04 19:43:46 -0500

    Styled yahtzee scoresheet a bit

--- client/src/components/games/Yahtzee.jsx
+++ client/src/components/games/Yahtzee.jsx
@@ -129,29 +129,29 @@ const Yahtzee = () => {
   const upperScoreSheet = (
       <div>
         <table className="table table-compact">
-          <caption className="font-bold text-primary text-lg/7">Upper Section</caption>
+          <caption className="font-bold text-primary text-lg/7 underline">Upper Section</caption>
           <thead>
-            <tr>
-              <th></th>
-              <th className="text-neutral">Aces</th>
-              <th className="text-neutral">Twos</th>
-              <th className="text-neutral">Threes</th>
-              <th className="text-neutral">Fours</th>
-              <th className="text-neutral">Fives</th>
-              <th className="text-neutral">Sixes</th>
+            <tr className="border border-primary">
+              <th className="border border-primary"></th>
+              <th className="text-neutral border border-primary">Aces</th>
+              <th className="text-neutral border border-primary">Twos</th>
+              <th className="text-neutral border border-primary">Threes</th>
+              <th className="text-neutral border border-primary">Fours</th>
+              <th className="text-neutral border border-primary">Fives</th>
+              <th className="text-neutral border border-primary">Sixes</th>
             </tr>
           </thead>
           <tbody>
             {data.players.map((player, index) => {
               return (
-                <tr key={player._id}>
-                  <th>{player.player_name}<input className="input ml-3 w-43" type="text" placeholder="edit player name" onChange={() => {handleChange(event.target.value, 'player_name', player.player_id)}}/></th>
-                  <td><p>{player.aces}</p><input className="input w-21" min='0' max='5' type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'aces', player.player_id)}/></td>
-                  <td><p>{player.twos}</p><input className="input w-21" min='0' max='10'  type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'twos', player.player_id)}/></td>
-                  <td><p>{player.threes}</p><input className="input w-21" min='0' max='15'  type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'threes', player.player_id)}/></td>
-                  <td><p>{player.fours}</p><input className="input w-21" min='0' max='20'  type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'fours', player.player_id)}/></td>
-                  <td><p>{player.fives}</p><input className="input w-21" min='0' max='25'  type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'fives', player.player_id)}/></td>
-                  <td><p>{player.sixes}</p><input className="input w-21" min='0' max='30'  type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'sixes', player.player_id)}/></td>
+                <tr key={player._id} className="border border-primary">
+                  <th><p>{player.player_name}</p><input className="input ml-3 w-43" type="text" placeholder="edit player name" onChange={() => {handleChange(event.target.value, 'player_name', player.player_id)}}/></th>
+                  <td className="border border-primary"><p>{player.aces}</p><input className="input w-21" min='0' max='5' type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'aces', player.player_id)}/></td>
+                  <td className="border border-primary"><p>{player.twos}</p><input className="input w-21" min='0' max='10'  type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'twos', player.player_id)}/></td>
+                  <td className="border border-primary"><p>{player.threes}</p><input className="input w-21" min='0' max='15'  type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'threes', player.player_id)}/></td>
+                  <td className="border border-primary"><p>{player.fours}</p><input className="input w-21" min='0' max='20'  type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'fours', player.player_id)}/></td>
+                  <td className="border border-primary"><p>{player.fives}</p><input className="input w-21" min='0' max='25'  type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'fives', player.player_id)}/></td>
+                  <td className="border border-primary"><p>{player.sixes}</p><input className="input w-21" min='0' max='30'  type='number' placeholder="0" onChange={() => handleChange(event.target.value, 'sixes', player.player_id)}/></td>
                 </tr>
               );
             })}
@@ -164,33 +164,33 @@ const Yahtzee = () => {
   const lowerScoreSheet = (
       <div>
         <table className="table table-compact">
-          <caption className="font-bold text-primary text-lg/7">Lower Section</caption>
+          <caption className="font-bold text-primary text-lg/7 underline">Lower Section</caption>
           <thead>
-            <tr>
-              <th></th>
-              <th className="text-neutral">3 of a Kind</th>
-              <th className="text-neutral">4 of a Kind</th>
-              <th className="text-neutral">Full House</th>
-              <th className="text-neutral">Small Straight</th>
-              <th className="text-neutral">Large Straight</th>
-              <th className="text-neutral">Yahtzee</th>
-              <th className="text-neutral">Chance</th>
-              <th className="text-neutral">Yahtzee Bonus</th>
+            <tr className="text-neutral border border-primary">
+              <th className="text-neutral border border-primary"></th>
+              <th className="text-neutral border border-primary">3 of a Kind</th>
+              <th className="text-neutral border border-primary">4 of a Kind</th>
+              <th className="text-neutral border border-primary">Full House</th>
+              <th className="text-neutral border border-primary">Small Straight</th>
+              <th className="text-neutral border border-primary">Large Straight</th>
+              <th className="text-neutral border border-primary">Yahtzee</th>
+              <th className="text-neutral border border-primary">Chance</th>
+              <th className="text-neutral border border-primary">Yahtzee Bonus</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="border border-primary">
             {data.players.map((player, index) => {
               return (
-                <tr key={player._id}>
-                  <th>{player.player_name}</th>
-                  <td><p>{player.three_of_a_kind}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => {handleChange(event.target.value, 'three_of_a_kind', player.player_id)}}/></td>
-                  <td><p>{player.four_of_a_kind}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'four', player.player_id)}/></td>
-                  <td><p>{player.full_house}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'full_house', player.player_id)}/></td>
-                  <td><p>{player.small_straight}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'small_straight', player.player_id)}/></td>
-                  <td><p>{player.large_straight}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'large_straight', player.player_id)}/></td>
-                  <td><p>{player.yahtzee}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'yahtzee', player.player_id)}/></td>
-                  <td><p>{player.chance}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'chance', player.player_id)}/></td>
-                  <td><p>{player.yahtzee_bonus}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'yahtzee_bonus', player.player_id)}/></td>
+                <tr key={player._id} className="border border-primary">
+                  <th className="w-250">{player.player_name}</th>
+                  <td className="border border-primary"><p>{player.three_of_a_kind}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => {handleChange(event.target.value, 'three_of_a_kind', player.player_id)}}/></td>
+                  <td className="border border-primary"><p>{player.four_of_a_kind}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'four', player.player_id)}/></td>
+                  <td className="border border-primary"><p>{player.full_house}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'full_house', player.player_id)}/></td>
+                  <td className="border border-primary"><p>{player.small_straight}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'small_straight', player.player_id)}/></td>
+                  <td className="border border-primary"><p>{player.large_straight}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'large_straight', player.player_id)}/></td>
+                  <td className="border border-primary"><p>{player.yahtzee}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'yahtzee', player.player_id)}/></td>
+                  <td className="border border-primary"><p>{player.chance}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'chance', player.player_id)}/></td>
+                  <td className="border border-primary"><p>{player.yahtzee_bonus}</p><input className="input w-21" type="number" min="0" placeholder="0" onChange={() => handleChange(event.target.value, 'yahtzee_bonus', player.player_id)}/></td>
                 </tr>
               );
             })}
@@ -200,19 +200,24 @@ const Yahtzee = () => {
   );
 
   return (
-    <div>
-      <h1>Yahtzee</h1>
-      <h3>Shareable Room Key: {data._id}</h3>
+    <div className="mt-20">
+      <h1 className="text-primary font-black text-xl/10 tracking-widest underline">Yahtzee</h1>
       <div>
-        <h2>Room Name: {data.room_name}</h2>
-        <div>
-          {upperScoreSheet}
+        <span className="text-primary font-bold text-lg/7">Shareable Room Key: </span>
+        <span className="font-bold text-lg/7 underline">{data._id}</span>
+      </div>
+      <div>
+        <h2 className="text-primary font-bold text-lg/7">Room Name: {data.room_name}</h2>
+        <div className="max-w-7xl mx-auto border-6 border-primary rounded-box p-4">
+          <div>
+            {upperScoreSheet}
+          </div>
+          <div className="divider bg-primary -ml-4 -mr-4"></div>
+          <div>
+            {lowerScoreSheet}
+          </div>
+          {saveButton ? <div><button className="btn btn-md btn-accent shadow-lg w-43" onClick={saveChanges}>Save Changes</button></div> : null}
         </div>
-        <div className="divider bg-primary"></div>
-        <div>
-          {lowerScoreSheet}
-        </div>
-        {saveButton ? <div><button className="btn btn-md btn-accent shadow-lg w-43" onClick={saveChanges}>Save Changes</button></div> : null}
       </div>
     </div>
   );
