6d65797b2c5828b3b2a085214cef42ba405158f7
Andrew Myronchuk <myronchukmail@gmail.com>
2025-03-07 20:10:41 -0500

    Game History unit tests

--- client/test/profile/Profile.test.jsx
+++ client/test/profile/Profile.test.jsx
@@ -126,3 +126,111 @@ describe('Profile Page', () => {
   });
 
 });
+
+
+describe('Game History', () => {
+
+  afterEach(() => {
+    mock.reset();
+  });
+
+  it ('should display "No games played" when games history is empty', async () => {
+
+    mock.onGet('/api/verifyLogin').reply(200,
+      'testuser@gmail.com'
+    );
+
+    mock.onGet('/api/profile/testuser@gmail.com').reply(200, [{
+      "profilePic": {
+          "data": null,
+          "contentType": null
+      },
+      "_id": "67ca25a6c8989b3eb7627502",
+      "username": "test",
+      "email": "testuser@gmail.com",
+      "phoneNumber": "",
+      "gamesInProgress": [],
+      "gamesPlayed": 0,
+      "gameHistory": [],
+      "friends": [],
+      "__v": 0
+    }]);
+
+    mock.onGet('/api/gameHistory/testuser@gmail.com').reply(200, []);
+
+    const profile = renderWithRouter(<Profile />);
+
+    await waitFor(() => {
+      expect(screen.getByText(/No games played/i)).toBeInTheDocument();
+    });
+
+  });
+
+  it ('should display game history when it is not empty', async () => {
+
+    mock.onGet('/api/verifyLogin').reply(200,
+      'testuser@gmail.com'
+    );
+
+    mock.onGet('/api/profile/testuser@gmail.com').reply(200, [{
+      "profilePic": {
+          "data": null,
+          "contentType": null
+      },
+      "_id": "67ca25a6c8989b3eb7627502",
+      "username": "test",
+      "email": "testuser@gmail.com",
+      "phoneNumber": "",
+      "gamesInProgress": [],
+      "gamesPlayed": 0,
+      "gameHistory": [],
+      "friends": [],
+      "__v": 0
+    }]);
+
+    mock.onGet('/api/gameHistory/testuser@gmail.com').reply(200, [
+      {
+          "_id": "67cb7038ad6ed1453aaddca1",
+          "winner": "Username",
+          "players": [
+              "Username",
+              "2",
+              "3"
+          ],
+          "game": "Clue",
+          "gameKey": "67cb7024ad6ed1453aaddc1a",
+          "email": "test5@gmail.com",
+          "createdAt": "2025-03-07T22:16:24.483Z",
+          "updatedAt": "2025-03-07T22:16:24.483Z",
+          "__v": 0
+      },
+      {
+          "_id": "67cb7099ad6ed1453aaddcd1",
+          "winner": "Player",
+          "players": [
+              "Player",
+              "Player"
+          ],
+          "game": "Scrabble",
+          "gameKey": "67cb7093ad6ed1453aaddcb7",
+          "email": "test5@gmail.com",
+          "createdAt": "2025-03-07T22:18:01.160Z",
+          "updatedAt": "2025-03-07T22:18:01.160Z",
+          "__v": 0
+      }
+  ]);
+
+    const profile = renderWithRouter(<Profile />);
+
+    await waitFor(() => {
+      expect(screen.getByText(/Games Played:\s+2/i)).toBeInTheDocument();
+      expect(screen.getByText(/Game:\s+Clue/i)).toBeInTheDocument();
+      expect(screen.getByText(/Game Key:\s+67cb7024ad6ed1453aaddc1a/i)).toBeInTheDocument();
+      expect(screen.getAllByText(/Date:\s+03\/07\/2025/i)).toHaveLength(2);
+      expect(screen.getByText(/Players:\s+Username, 2, 3/i)).toBeInTheDocument();
+      expect(screen.getByText(/Winner:\s+Username/i)).toBeInTheDocument();
+    });
+
+  });
+
+});
